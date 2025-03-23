47e8b1deb9ac49965786651c8d49f2dbbe082e58
River <revir.qing@gmail.com>
2025-03-23 12:19:16 +0100

    font selector component, add some more handwriting fonts

---
+++ src/content/fontSelector.js
@@ -0,0 +1,141 @@
+import { h } from "preact";
+import htm from "htm";
+import { useEffect } from "preact/hooks";
+
+const html = htm.bind(h);
+
+const FontSelector = ({ settings, saveSettings }) => {
+  const fontType = settings.fontType || "system-ui";
+  const changeFontType = (e) => {
+    const newFontType = e.target.value;
+    const $article = document.getElementById("PNLReaderArticle");
+    if ($article) {
+      const fontFamily_ = `${newFontType}, system-ui, Segoe UI, Roboto, Helvetica, Arial, sans-serif`;
+      $article.style.setProperty("--pico-font-family", fontFamily_);
+      saveSettings({ fontType: newFontType });
+    }
+  };
+
+  // init the font family
+  useEffect(() => {
+    changeFontType({ target: { value: fontType } });
+  }, []);
+
+  const allFonts = [
+    { value: "system-ui", label: "System UI" },
+    {
+      value: "'Source Serif 4'",
+      label: "Source Serif 4",
+    },
+    {
+      value: "'Segoe UI'",
+      label: "Segoe UI",
+    },
+    {
+      value: "Calibri",
+      label: "Calibri",
+    },
+    {
+      value: "Georgia",
+      label: "Georgia",
+    },
+    {
+      value: "Merriweather",
+      label: "Merriweather",
+    },
+    {
+      value: "'Times New Roman'",
+      label: "Times New Roman",
+    },
+    {
+      value: "Roboto",
+      label: "Roboto",
+    },
+    {
+      value: "'Open Sans'",
+      label: "Open Sans",
+    },
+    {
+      value: "Fredoka",
+      label: "Fredoka",
+    },
+    {
+      value: "'Baloo 2'",
+      label: "Baloo 2",
+    },
+    {
+      value: "'Source Code Pro', monospace",
+      label: "Source Code Pro",
+    },
+    {
+      value: "'IBM Plex Mono', monospace",
+      label: "IBM Plex Mono",
+    },
+    {
+      value: "'Comic Sans MS', cursive",
+      label: "Comic Sans MS",
+    },
+    {
+      value: "'Patrick Hand'",
+      label: "Patrick Hand",
+    },
+
+    {
+      value: "'Gloria Hallelujah'",
+      label: "Gloria Hallelujah",
+    },
+    {
+      value: "Lobster, cursive",
+      label: "Lobster",
+    },
+    {
+      value: "'Indie Flower', cursive",
+      label: "Indie Flower",
+    },
+    {
+      value: "'Dancing Script', cursive",
+      label: "Dancing Script",
+    },
+    {
+      value: "Caveat, cursive",
+      label: "Caveat",
+    },
+    {
+      value: "Chewy",
+      label: "Chewy",
+    },
+    {
+      value: "'Permanent Marker'",
+      label: "Permanent Marker",
+    },
+    {
+      value: "'Luckiest Guy'",
+      label: "Luckiest Guy",
+    },
+    {
+      value: "Bangers",
+      label: "Bangers",
+    },
+  ];
+
+  return html`
+    <select
+      id="fontType"
+      name="fontType"
+      aria-label="Select Font"
+      onChange=${changeFontType}
+      value=${fontType}
+    >
+      ${allFonts.map(
+        (font) =>
+          html`
+            <option value=${font.value} style="font-family: ${font.value};">
+              ${font.label}
+            </option>
+          `
+      )}
+    </select>
+  `;
+};
+
+export default FontSelector;
--- src/content/textStylesDropdown.js
+++ src/content/textStylesDropdown.js
@@ -1,28 +1,12 @@
 import { h } from "preact";
-import { useEffect, useRef } from "preact/hooks";
 import htm from "htm";
 import "./fontello-embedded.scss";
 import FontSizeRange from "./fontSizeRange.js";
+import FontSelector from "./fontSelector.js";
 import "./googleFonts/fonts.css";
 const html = htm.bind(h);
 
 const TextStylesDropdown = ({ settings, saveSettings }) => {
-  const fontType = settings.fontType || "system-ui";
-  const changeFontType = (e) => {
-    const newFontType = e.target.value;
-    const $article = document.getElementById("PNLReaderArticle");
-    if ($article) {
-      const fontFamily_ = `${newFontType}, Roboto, Oxygen, Ubuntu, Cantarell, Helvetica, Arial, sans-serif`;
-      $article.style.setProperty("--pico-font-family", fontFamily_);
-      saveSettings({ fontType: newFontType });
-    }
-  };
-
-  // init the font family
-  useEffect(() => {
-    changeFontType({ target: { value: fontType } });
-  }, []);
-
   return html`
     <details class="dropdown">
       <summary role="button" class="outline secondary">
@@ -36,99 +20,10 @@ const TextStylesDropdown = ({ settings, saveSettings }) => {
               <span class="pnl-icon icon-fontsize" title="Type controls"></span>
               <span>Font Type</span>
 
-              <!-- Select -->
-              <select
-                id="fontType"
-                name="fontType"
-                aria-label="Select Font"
-                onChange=${changeFontType}
-                value=${fontType}
-              >
-                <option value="system-ui" style="font-family: system-ui;">
-                  System UI
-                </option>
-                <option
-                  value="'Source Serif 4'"
-                  style="font-family: 'Source Serif 4';"
-                >
-                  Source Serif 4
-                </option>
-                <option value="'Segoe UI'" style="font-family: 'Segoe UI';">
-                  Segoe UI
-                </option>
-                <option value="Calibri" style="font-family: Calibri;">
-                  Calibri
-                </option>
-                <option value="Georgia" style="font-family: Georgia;">
-                  Georgia
-                </option>
-                <option value="Merriweather" style="font-family: Merriweather;">
-                  Merriweather
-                </option>
-                <option
-                  value="'Times New Roman'"
-                  style="font-family: 'Times New Roman';"
-                >
-                  Times New Roman
-                </option>
-                <option value="Roboto" style="font-family: Roboto;">
-                  Roboto
-                </option>
-                <option value="'Open Sans'" style="font-family: 'Open Sans';">
-                  Open Sans
-                </option>
-                <option
-                  value="'Courier New'"
-                  style="font-family: 'Courier New';"
-                >
-                  Courier New
-                </option>
-                <option
-                  value="'Comic Sans MS', cursive"
-                  style="font-family: 'Comic Sans MS', cursive;"
-                >
-                  Comic Sans MS
-                </option>
-                <option
-                  value="'Patrick Hand'"
-                  style="font-family: 'Patrick Hand';"
-                >
-                  Patrick Hand
-                </option>
-                <option value="Fredoka" style="font-family: Fredoka;">
-                  Fredoka
-                </option>
-                <option value="'Baloo 2'" style="font-family: 'Baloo 2';">
-                  Baloo 2
-                </option>
-                <option value="'Chewy'" style="font-family: 'Chewy';">
-                  Chewy
-                </option>
-                <option
-                  value="'Gloria Hallelujah'"
-                  style="font-family: 'Gloria Hallelujah';"
-                >
-                  Gloria Hallelujah
-                </option>
-                <option
-                  value="'Permanent Marker'"
-                  style="font-family: 'Permanent Marker';"
-                >
-                  Permanent Marker
-                </option>
-                <option
-                  value="'Luckiest Guy'"
-                  style="font-family: 'Luckiest Guy';"
-                >
-                  Luckiest Guy
-                </option>
-                <option value="Bangers" style="font-family: Bangers;">
-                  Bangers
-                </option>
-                <option value="Caveat" style="font-family: Caveat;">
-                  Caveat
-                </option>
-              </select>
+              <${FontSelector}
+                settings=${settings}
+                saveSettings=${saveSettings}
+              />
             </div>
             <div class="list-in-row show-xs">
               <!-- Settings for fontsize -->
