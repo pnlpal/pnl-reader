b0cd439c2b979bebb3304c087c2f0481ea1eb84a
River <revir.qing@gmail.com>
2025-10-25 21:18:26 +0200

    highlight content can fail due to non-text nodes

--- src/content/ttsPlayer/highlightSelection.js
+++ src/content/ttsPlayer/highlightSelection.js
@@ -12,7 +12,13 @@ function highlightSelection(selection) {
   const range = selection.getRangeAt(0);
   const span = document.createElement("span");
   span.className = "tts-selected-sentence";
-  range.surroundContents(span);
+
+  try {
+    range.surroundContents(span);
+  } catch (e) {
+    console.error("Failed to highlight selection:", e);
+    // If surroundContents fails (e.g., due to partial selection of non-text nodes), ignore highlighting.
+  }
 }
 
 export { highlightSelection, clearHighlights };
